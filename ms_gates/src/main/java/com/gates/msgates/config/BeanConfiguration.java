6e3d4837cb8d58b2bbe8890130ed816674b47cab
wccm92 <wccm92@gmail.com>
2026-05-13 20:33:42 -0500

    fix: corrección bean

--- ms_gates/src/main/java/com/gates/msgates/config/BeanConfiguration.java
+++ ms_gates/src/main/java/com/gates/msgates/config/BeanConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.RestClient;
 
@@ -80,6 +81,7 @@ public class BeanConfiguration {
     }
 
     @Bean
+    @Primary
     public VisitorRepositoryPort visitorRepositoryPort(JdbcVisitorRepository localVisitorRepository,
                                                        RemoteJdbcVisitorRepository remoteVisitorRepository) {
         return new CompositeVisitorRepository(localVisitorRepository, remoteVisitorRepository);
